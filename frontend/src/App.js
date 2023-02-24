01afedcadf230124c3b67f68c234f2e84cc4b8aa
halaszruben <ruben_halasz12@yahoo.com>
2023-02-24 12:47:32 +0200

    Added PrivateRoutes and started Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,51 @@
 
 import { useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { useLocalState } from './util/useLocalStorage';
+import Dashboard from './Dashboard'
+import Homepage from './Homepage'
+import Login from './Login';
+import PrivateRoute from './PrivateRoute';
 
 function App() {
   const [jwt, setJwt] = useLocalState("", "jwt");
 
-  useEffect(() => {
-    if (!jwt) {
-      const reqBody = {
-        username: "trevor",
-        password: "asdfasdf",
-      };
+  // useEffect(() => {
+  //   if (!jwt) {
+  //     const reqBody = {
+  //       username: "trevor",
+  //       password: "asdfasdf",
+  //     };
 
-      fetch("api/auth/login", {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        method: "post",
-        body: JSON.stringify(reqBody),
-      })
-        .then((response) => Promise.all([response.json(), response.headers]))
-        .then(([body, headers]) => {
-          setJwt(headers.get("authorization"));
-        });
-    }
-  }, []);
+  //     fetch("api/auth/login", {
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       method: "post",
+  //       body: JSON.stringify(reqBody),
+  //     })
+  //       .then((response) => Promise.all([response.json(), response.headers]))
+  //       .then(([body, headers]) => {
+  //         setJwt(headers.get("authorization"));
+  //       });
+  //   }
+  // }, []);
 
   useEffect(() => {
     console.log(`JWT is: ${jwt}`);
   }, [jwt]);
 
   return (
-    <div className="App">
-      <h1>HELLO</h1>
-    </div>
+    <Routes>
+      <Route path="/dashboard" element={
+        <PrivateRoute>
+          <Dashboard />
+        </PrivateRoute>
+      } />
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Homepage />} />
+    </Routes>
   );
 }
 
