37bee4d30cae8fdf8af693dbc4871505dcead6e8
halaszruben <ruben_halasz12@yahoo.com>
2023-02-28 11:47:41 +0200

    Statuses are in order

--- frontend/src/CustomerDashboard/index.js
+++ frontend/src/CustomerDashboard/index.js
@@ -14,7 +14,6 @@ const CustomerDashboard = () => {
 
         const user = {
             username: decodedJwt.sub,
-            authorities: decodedJwt.authorities,
         };
 
         table.tableClaimer = user;
@@ -22,8 +21,11 @@ const CustomerDashboard = () => {
         table.status = "Table is occupied";
 
         ajax(`api/tables/${table.id}`, "PUT", jwt, table)
-            .then(table => {
-                //TODO: view update
+            .then(updatedTable => {
+                const tablesCopy = [...tables];
+                const i = tablesCopy.findIndex((t) => t.id === table.id);
+                tablesCopy[i] = updatedTable;
+                setTables(tablesCopy);
             });
     }
 
@@ -44,7 +46,7 @@ const CustomerDashboard = () => {
                         style={{ cursor: "pointer", fontSize: "large", color: "burgundy" }}
                         onClick={() => {
                             setJwt(null);
-                            window.location.href = "/login"
+                            window.location.href = "/login";
                         }}
                     >
                         Logout
@@ -64,63 +66,158 @@ const CustomerDashboard = () => {
                     Free Tables
                 </div>
 
-                {tables ? (
+                {(tables && tables.filter((table) => table.status === "Table is free").length > 0) ? (
                     <div
                         className="d-grid gap-5"
                         style={{ gridTemplateColumns: "repeat(auto-fit, 18rem)" }}
                     >
-                        {tables.map((table) => (
-                            <Card
-                                key={table.id}
-                                style={{ width: "18rem", height: "32rem" }}
-                            >
-                                <Card.Body
-                                    className="d-flex flex-column justify-content-around">
-                                    <Card.Title
-                                        className="tableNumber"
-                                    >
-                                        Table #{table.assignedNumber}
-                                    </Card.Title>
-                                    <div>
-                                        <Badge
-                                            pill
-                                            bg="success"
-                                            style={{ fontSize: "1em" }}
+                        {tables.filter((table) => table.status === "Table is free")
+                            .map((table) => (
+                                <Card
+                                    key={table.id}
+                                    style={{ width: "18rem", height: "32rem" }}
+                                >
+                                    <Card.Body
+                                        className="d-flex flex-column justify-content-around">
+                                        <Card.Title
+                                            className="tableNumber"
                                         >
-                                            {table.status}
-                                        </Badge>
-                                    </div>
-                                    <Card.Text style={{ marginTop: "1em" }}>
-                                        <p>
-                                            <b>Description</b>: {table.description}
-                                        </p>
-                                        <p>
-                                            <b>Number of seating places</b>: {table.chairs}
-                                        </p>
-                                    </Card.Text>
+                                            Table #{table.assignedNumber}
+                                        </Card.Title>
+                                        <div>
+                                            <Badge
+                                                pill
+                                                bg="success"
+                                                style={{ fontSize: "1em" }}
+                                            >
+                                                {table.status}
+                                            </Badge>
+                                        </div>
+                                        <Card.Text style={{ marginTop: "1em" }}>
+                                            <p>
+                                                <b>Description</b>: {table.description}
+                                            </p>
+                                            <p>
+                                                <b>Number of seating places</b>: {table.chairs}
+                                            </p>
+                                        </Card.Text>
 
-                                    <Button
-                                        variant="primary"
-                                        onClick={() => {
-                                            claimTable(table);
-                                        }}
-                                    >
-                                        Sit down
-                                    </Button>
+                                        <Button
+                                            variant="primary"
+                                            onClick={() => {
+                                                claimTable(table);
+                                            }}
+                                        >
+                                            Sit down
+                                        </Button>
 
-                                </Card.Body>
-                            </Card>
-                        ))}
+                                    </Card.Body>
+                                </Card>
+                            ))}
                     </div>
                 ) : (
-                    <></>
+                    <div>Currently there are no free tables</div>
                 )}
             </div>
 
-            {/* <div className="table-wrapper occupied-table"></div> */}
+            <div className="table-wrapper occupied-table">
+                <div
+                    className="table-statuses h2 px-2">
+                    Occupied Tables
+                </div>
+                {(tables && tables.filter((table) => table.status === "Table is occupied").length > 0) ? (
+                    <div
+                        className="d-grid gap-5"
+                        style={{ gridTemplateColumns: "repeat(auto-fit, 18rem)" }}
+                    >
+                        {tables.filter((table) => table.status === "Table is occupied")
+                            .map((table) => (
+                                <Card
+                                    key={table.id}
+                                    style={{ width: "18rem", height: "32rem" }}
+                                >
+                                    <Card.Body
+                                        className="d-flex flex-column justify-content-around">
+                                        <Card.Title
+                                            className="tableNumber"
+                                        >
+                                            Table #{table.assignedNumber}
+                                        </Card.Title>
+                                        <div>
+                                            <Badge
+                                                pill
+                                                bg="success"
+                                                style={{ fontSize: "1em" }}
+                                            >
+                                                {table.status}
+                                            </Badge>
+                                        </div>
+                                        <Card.Text style={{ marginTop: "1em" }}>
+                                            <p>
+                                                <b>Description</b>: {table.description}
+                                            </p>
+                                            <p>
+                                                <b>Number of seating places</b>: {table.chairs}
+                                            </p>
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            ))}
+                    </div>
+                ) : (
+                    <div>There are no occupied tables</div>
+                )}
+            </div>
 
-            {/* <div className="table-wrapper unavailable-table"></div> */}
+            <div className="table-wrapper unavailable-table">
+                <div
+                    className="table-statuses h2 px-2">
+                    Unavailable Tables
+                </div>
+                {(tables && tables.filter((table) => table.status === "Unavailable").length > 0) ? (
+                    <div
+                        className="d-grid gap-5"
+                        style={{ gridTemplateColumns: "repeat(auto-fit, 18rem)" }}
+                    >
+                        {tables.filter((table) => table.status === "Unavailable")
+                            .map((table) => (
+                                <Card
+                                    key={table.id}
+                                    style={{ width: "18rem", height: "32rem" }}
+                                >
+                                    <Card.Body
+                                        className="d-flex flex-column justify-content-around">
+                                        <Card.Title
+                                            className="tableNumber"
+                                        >
+                                            Table #{table.assignedNumber}
+                                        </Card.Title>
+                                        <div>
+                                            <Badge
+                                                pill
+                                                bg="success"
+                                                style={{ fontSize: "1em" }}
+                                            >
+                                                {table.status}
+                                            </Badge>
+                                        </div>
+                                        <Card.Text style={{ marginTop: "1em" }}>
+                                            <p>
+                                                <b>Description</b>: {table.description}
+                                            </p>
+                                            <p>
+                                                <b>Number of seating places</b>: {table.chairs}
+                                            </p>
+                                        </Card.Text>
 
+                                    </Card.Body>
+                                </Card>
+                            ))}
+                    </div>
+                ) : (
+                    <div>All the tables are available for customers</div>
+                )}
+            </div>
 
         </Container>
     );
